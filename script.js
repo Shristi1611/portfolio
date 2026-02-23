fa152651f0517ba3179e2989b7559f43fba01985
Shristi1611 <shristisinha59@gmail.com>
2026-02-23 16:38:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -87,3 +87,4 @@ document.addEventListener("DOMContentLoaded", type);
 let topBtn = document.getElementById("backToTop");
 window.onscroll = () => { topBtn.style.display = (window.scrollY > 300) ? "block" : "none"; };
 topBtn.onclick = () => window.scrollTo({top: 0, behavior: 'smooth'});
+
